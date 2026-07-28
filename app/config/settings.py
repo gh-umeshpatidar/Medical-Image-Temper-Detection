99b07731b0a397ab32f3267c2d85455f675c8e02
Umesh Patidar <umeshmukaddam@gmail.com>
2026-07-28 19:04:19 +0530

    modifed setting.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,7 +2,7 @@ import os
 
 import torch
 
-BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 MODEL_PATH = os.path.join(BASE_DIR, "ml_models", "best_model.pth")
 
